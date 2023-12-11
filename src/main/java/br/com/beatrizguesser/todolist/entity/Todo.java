fab85e59af4ee000c8ea9e42f2b2fda406505366
Beatriz <biabagg12@gmail.com>
2023-12-11 18:29:57 -0300

    Added tests and swagger

--- src/main/java/br/com/beatrizguesser/todolist/entity/Todo.java
+++ src/main/java/br/com/beatrizguesser/todolist/entity/Todo.java
@@ -1,6 +1,7 @@
 package br.com.beatrizguesser.todolist.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,17 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String nome;
+    @NotBlank
     private String descricao;
     private boolean realizado;
     private int prioridade;
+
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
 }
